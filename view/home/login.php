f58cce434092f63cab03ef44b9cb8d2b3614df17
godkarm <godkarm@gmail.com>
2023-05-20 14:13:57 -0500

    dando forma al login

--- view/home/login.php
+++ view/home/login.php
@@ -14,12 +14,17 @@
     <form action="verificar.php" method="POST" class="col-3 login" autocomplete="off">
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <input type="email" name="correo" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <div class="box-eye">
+                <button type="button" onclick="mostrarContraseña(pasword)">
+                    <i id="eyepassword" class="fa-solid fa-eye changePassword"></i>
+                </button>
+            </div>
+            <input type="password" name="contraseña" class="form-control" id="exampleInputPassword1">
         </div>
         <!-- <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
